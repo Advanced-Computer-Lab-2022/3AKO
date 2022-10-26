bea47f343aa042f72a99bf55d682ca789e8fc406
ZerooCool <adhmm0928@gmail.com>
2022-10-26 20:25:29 +0200

    changed the individual trainee schema

--- backend/routes/individualTraineeRouter.js
+++ backend/routes/individualTraineeRouter.js
@@ -5,10 +5,10 @@ const individualTraineeModel = require('../models/individualTraineeModel')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-    const {username, password, firstName, lastName, email, gender, country} = req.body
+    const {username, password, firstName, lastName, email, gender, country, coursesList} = req.body
 
     try {
-        const individualTrainee = await individualTraineeModel.create({username, password, firstName, lastName, email, gender, country}) 
+        const individualTrainee = await individualTraineeModel.create({username, password, firstName, lastName, email, gender, country, coursesList}) 
         res.status(200).json(individualTrainee)
     }catch(err){
         res.status(400).json({error : err.message})
