164e52224f86f6296bf64fee9e68c2f2253dc15f
Aboda81 <abdo.hussein812@gmail.com>
2022-10-26 11:14:18 +0200

    course model added

---
+++ backend/models/courseModel.js
@@ -0,0 +1,162 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const lessonSchema = mongoose.Schema({
+    title : {
+        type : String,
+        required: true
+    },
+    description :{
+        type : String
+    }
+    ,
+    videoURL :{
+        type : URL,
+        required : true
+    },
+    readings :{
+        type : String,
+    }
+})
+
+const questionSchema = mongoose.Schema({
+    question : {
+        type : String,
+        require : true
+    },
+    answer1 : {
+        value : {
+            type : String,
+            required : true
+
+        },
+        isTrue : {
+            type :  Boolean,
+            required : true
+        }
+    },
+    answer2 : {
+        value : {
+            type : String,
+            required : true
+
+        },
+        isTrue : {
+            type :  Boolean,
+            required : true
+        }
+    },
+    answer3 : {
+        value : {
+            type : String,
+            required : true
+
+        },
+        isTrue : {
+            type :  Boolean,
+            required : true
+        }
+    },
+    answer4 : {
+        value : {
+            type : String,
+            required : true
+
+        },
+        isTrue : {
+            type :  Boolean,
+            required : true
+        }
+    }   
+
+})
+
+const quizSchema = mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    questions :{
+        type : [questionSchema],
+        required : true
+    }
+
+})
+
+const subtitleSchema =mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    lessons :{
+        type : [lessonSchema,quizSchema],
+        required : true
+    }
+
+    
+})
+const courseSchema = new Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    outline :{
+        type: String,
+        required : true
+    },
+    summary : {
+        type : String,
+        required: true
+
+    },
+    previewVideo :{
+        type : URL,
+        required : true
+
+    },
+    subject : {
+        type : String,
+        required : true
+    },
+    subtitles : {
+       type : [subtitleSchema],
+       required : true
+    },
+    rating : {
+        type : Number,
+        min : 0,
+        max : 5
+    },
+    price : {
+        type : Number,
+        min : 0,
+        required : true
+    },
+    totalHours : {
+        type : Number,
+        required : true
+    },
+    instrucrtorId : {
+        type : mongoose.ObjectId,
+        required : true
+    },
+    promotion :{
+        sale : {
+            type : Number
+        },
+        byInstructor :{
+            type : Boolean,
+            required : true
+        }
+    },
+    numOfViews : {
+        type : Number,
+        required : true,
+        default : 0,
+    }
+    
+    
+}, {timestamps : true})
+
+module.exports = mongoose.model('course', courseSchema)
