f5112765c065929156299b05099c92728403f72f
ZerooCool <adhmm0928@gmail.com>
2022-10-30 21:54:38 +0200

    add the flags and county selection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <CountryModal/>
+      <CountryModal />
       <CourseView courseId={"635977a729582539eb13961e"} currency={"$"}></CourseView>
     </div>
     
