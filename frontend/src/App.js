8add46a9a1b63635e6a278df190417bdc7efc0a9
AhmedHatem0 <ahmedhateem315@gmail.com>
2022-10-30 17:45:07 +0200

    created empty instructorCourseView

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import CourseView from './CourseView';
-
+import InstructorCourses from './instructorCoursesView';
 function App() {
   return (
     <div className="App">
+      <InstructorCourses />
       <h2>"welcome Home"</h2>
     </div>
     
---
+++ frontend/src/instructorCoursesView.js
@@ -0,0 +1,10 @@
+const InstructorCourses = () => {
+    return ( 
+    <div className="instructor-courses">
+        <title>show courses</title>
+        <button>show instructor courses</button>
+        <h2> hello</h2>
+    </div> );
+}
+ 
+export default InstructorCourses;
