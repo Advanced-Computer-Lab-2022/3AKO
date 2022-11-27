84934d7e1ea3da24f00fdd2ca3fef5a187941772
osama <osamahossinyali@gmail.com>
2022-11-27 05:20:17 +0200

    Models update p3

--- backend/models/corporateTraineeModel.js
+++ backend/models/corporateTraineeModel.js
@@ -2,43 +2,11 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const courseList = new Schema({
-    courseId : {
-        type : mongoose.Schema.Types.ObjectId,
-        required : true
-    },
-    progress : {
-        type : Number,
-        min : 0,
-        max : 100,
-        default : 0
-    },
-    exercisesList : {
-        type: [{
-            exercisesId:{ type : mongoose.ObjectId, ref: 'exercise'}, 
-            grade:{ type : Number, required : true},
-            answers : {type : [Number],required : true}}]
-    },
-    lessonsList : {
-        type: [{
-            lessonId:{ type : mongoose.ObjectId, ref: 'lesson'},
-            note:{type : String,default : ""}}]
-    }
-
-
-    
-
-},{autoCreate : false, _id : false})
-
 const corporateTraineeSchema = new Schema({
     _id : {
         type : mongoose.ObjectId,
         require : true,
-        ref : "user"
-    },
-    courseList : {
-        type : [courseList],
-        default : []
+        ref : "trainee"
     },
     courseRequests : {
         type : [{
@@ -51,26 +19,11 @@ const corporateTraineeSchema = new Schema({
                     default : "pendding" 
                 }
             }
-        ]
-    }
-    ,
-    complaints : {
-        type : [mongoose.ObjectId],
+        ],
         default : []
-    },
-    gender:{
-        type : String,
-        required : false
-    },
-    name:{
-        type : String,
-        required : true
     }
-    
 })
 const corporateTrainee = mongoose.model('corporateTrainee' , corporateTraineeSchema)
-const courseRecordModel = mongoose.model('courseRecordModel' ,courseList) 
 module.exports = {
-    corporateTrainee,
-    courseRecordModel
+    corporateTrainee
 }
