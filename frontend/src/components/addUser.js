c423e89133f42d83b4e4bb9998f881e8d1a952f6
ZerooCool <adhmm0928@gmail.com>
2022-12-18 00:41:44 +0200

    add admin add user

--- frontend/src/components/addUser.js
+++ frontend/src/components/addUser.js
@@ -1,9 +1,34 @@
 
 import axios from "axios";
 import { useState } from "react";
+import React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import MenuIcon from "@mui/icons-material/Menu";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import {
+    AppBar,
+    CssBaseline,
+    FormControl,
+    IconButton,
+    InputLabel,
+    MenuItem,
+    NativeSelect,
+    Select,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+const AddUser = () => {
+    const [userType, setUserType] = useState("Admin");
 
-const AddUser = ({fields, userType}) => {
-    
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [email, setEmail] = useState('')
@@ -27,51 +52,66 @@ const AddUser = ({fields, userType}) => {
     }
 
     return (
-        <form className="addUser" onSubmit = {handleAdd}>
+        <form className="addUser" onSubmit={handleAdd}>
+            <Box>
+                <Typography>Select user type</Typography>
+
+                <Select
+                    defaultValue={"Admin"}
+                   
+                    onChange={(e) => {
+                        setUserType(e.target.value);
+                    }}
+                >
+                    <MenuItem value={"Admin"}>Admin</MenuItem>
+                    <MenuItem value={"Instructor"}>Instructor</MenuItem>
+                    <MenuItem value={"CorporateTrainee"}>
+                        Corporate Trainee
+                    </MenuItem>
+                </Select>
+            </Box>
+
             <h3>add {userType}</h3>
-          
+
             <div>
                 <label>Username</label>
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     onChange={(e) => setUsername(e.target.value)}
-                    value = {username}
-                    required />
+                    value={username}
+                    required
+                />
             </div>
-            
 
-           <div>
-            <label>Password</label>
-                <input 
-                    type="password" 
+            <div>
+                <label>Password</label>
+                <input
+                    type="password"
                     onChange={(e) => setPassword(e.target.value)}
-                    value = {password}
-                    required />
-                    
-           </div>
+                    value={password}
+                    required
+                />
+            </div>
 
-            {fields === 3 && <div>
-                <label>Email</label>
-                <input 
-                    type="email" 
-                    onChange={(e) => setEmail(e.target.value)}
-                    value = {email}
-                    required />
-            </div>}
-
-            <button>Add {userType}</button> 
-
-            {success && 
+            {userType !== "Admin" && (
                 <div>
-                    {success}
-                </div>}
-            
-            {error && 
-                <div className="error">
-                    {error}
-                </div>}
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        required
+                    />
+                </div>
+            )}
+
+            <button>Add {userType}</button>
+
+            {success && <div>{success}</div>}
+
+            {error && <div className="error">{error}</div>}
         </form>
-    )
+    );
 }
 
 export default AddUser;
