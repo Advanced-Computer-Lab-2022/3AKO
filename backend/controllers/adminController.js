867ee1c3c88529511e9521d0f0c5769779bfd64e
osama <osamahossinyali@gmail.com>
2023-01-02 03:10:19 +0200

    fixed refund and added send certificate

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -124,27 +124,47 @@ const answerRefundRequest = async (req, res) => {
     try {
         const { requestId, response } = req.body;
         const request = await refundRequests.findOne({ _id: requestId });
+        console.log(requestId, response);
         if (!request)
             return res.status(400).json({ error: "invalid request id" });
         if (response === "accept") {
             // get the payment details to get the paid amount
             const paymentData = await individualTraineeModel.findOne({_id:request.traineeId},{wallet:0,_id:0,payments: { $elemMatch: { courseId: request.courseId,status:'completed' }}}).lean()
+            console.log({paymentData});
             const payment = paymentData.payments[0]
+            console.log({payment});
+
             // get the instructor id from the course data
             const courseData = await courseModel.findOne({_id:request.courseId},'instructorId -_id').lean()
+            console.log({courseData});
+
             // add the refunded amount to the trainee wallet
             await individualTraineeModel.updateOne({_id:request.traineeId},{$inc:{wallet:payment.amount}},{new:true,upsert:true})
+            console.log({terror:"reached"});
+
             // get the cut for the instructor
             const precentageData = await agreementModel.findOne({},'instructorCut -_id').lean()
             const percentage = precentageData.instructorCut
             const now = new Date()
             const date = new Date(now.getFullYear(), now.getMonth(), 1)
             // deduct the amount given to the instructor from the current month
-            await instructorModel.updateOne({_id : courseData.instructorId,'earnings.startDate':date},{$inc:{'earnings.$.sum': -percentage/100*payment.amount}},{new:true,upsert:true})
+            //await instructorModel.updateOne({_id : courseData.instructorId,'earnings.startDate':date.toISOString()},{$inc:{'earnings.$.sum': -percentage/100*payment.amount}},{new:true,upsert:true})
+            const instructorData = await instructorModel.findOne({ _id: courseData.instructorId }, { earnings: 1, _id: 0, rating: 0, courses: 0, name: 0, gender: 0, biography: 0, consent: 0, reviews: 0, earnings: { $elemMatch: { startDate: date.toISOString() } } }).lean()
+            // add the amount owed for the instructor to the current month earnings record if exists
+            if (instructorData.earnings && instructorData.earnings.length) {
+            const newData = await instructorModel.updateOne({ _id: courseData.instructorId, 'earnings.startDate': date }, { $inc: { 'earnings.$.sum':-percentage/100*payment.amount} }, { new: true, upsert: true })
+            }
+            else { // create a record for the month and add the owed amount to it if one does not exist
+            const newData = await instructorModel.updateOne({ _id: courseData.instructorId }, { $push: { earnings: { startDate: date, sum: -percentage/100*payment.amount } } }, { new: true, upsert: true })
+            }
+            console.log("ok");
+            // change the status of the course
+                await traineeModel.updateOne({_id:request.traineeId,'courseList.courseId':request.courseId},{'courseList.$.status':"refunded"},{new:true,upsert:true});
+            
             
             // mark the request as accepted
             await refundRequests.updateOne({ _id: requestId }, { status: "accepted" }, { new: true, upsert: true });
-                    
+                    console.log("not ok");
             
             res.status(200).json({ message: "successful" });
         } else if (response === "reject") {
